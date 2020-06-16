01fa537cc8d4e80dbe29dab129a1f5943e549731
Max Shcherbinin <maksimserbinin9119@gmail.com>
2020-06-16 09:15:08 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //});
 Route::resource('/passports', 'PassportController');
-Route::get('/post', 'SearchController');
+Route::post('/search', 'SearchController');
